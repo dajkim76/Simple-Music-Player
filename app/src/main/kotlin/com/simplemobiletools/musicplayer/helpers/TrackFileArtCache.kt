03c66812dc973a2e2e9065599bb8fcb920a473e7
dajkim76 <dajkim76@naver.com>
2026-03-06 13:33:40 +0900

    Using file cache keys is problematic because MediaStore IDs can be reused. It's implemented only as a memory cache.

--- app/src/main/kotlin/com/simplemobiletools/musicplayer/helpers/TrackFileArtCache.kt
+++ app/src/main/kotlin/com/simplemobiletools/musicplayer/helpers/TrackFileArtCache.kt
@@ -3,14 +3,9 @@ package com.simplemobiletools.musicplayer.helpers
 import android.app.ActivityManager
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.util.LruCache
-import java.io.File
-import java.io.FileOutputStream
 
 class TrackFileArtCache private constructor(private val context: Context) {
-    private val cacheDir = File(context.cacheDir, "track_art_cache").apply { mkdirs() }
-
     private val bitmapCache: LruCache<Long, Bitmap> = object : LruCache<Long, Bitmap>(getCacheSize()) {
         override fun sizeOf(key: Long, value: Bitmap): Int {
             return value.byteCount
@@ -27,43 +22,11 @@ class TrackFileArtCache private constructor(private val context: Context) {
 
     @Synchronized
     fun put(key: Long, bitmap: Bitmap) {
-        if (bitmapCache.get(key) == null) {
-            bitmapCache.put(key, bitmap)
-        }
-
-        val file = File(cacheDir, makeFilename(key))
-        if (!file.exists()) {
-            try {
-                FileOutputStream(file).use { out ->
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
+        bitmapCache.put(key, bitmap)
     }
 
     @Synchronized
     fun get(key: Long): Bitmap? {
-        var bitmap = bitmapCache.get(key)
-        if (bitmap == null) {
-            val file = File(cacheDir, makeFilename(key))
-            if (file.exists()) {
-                try {
-                    bitmap = BitmapFactory.decodeFile(file.absolutePath)
-                    if (bitmap != null) {
-                        bitmapCache.put(key, bitmap)
-                    }
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }
-        }
-        return bitmap
-    }
-
-    @Synchronized
-    fun peek(key: Long): Bitmap? {
         return bitmapCache.get(key)
     }
 
@@ -76,8 +39,6 @@ class TrackFileArtCache private constructor(private val context: Context) {
     fun setNoEmbeddedPicture(key: Long) {
         noEmbeddedPictureMap[key] = true
     }
-
-    private fun makeFilename(key: Long) = "track_$key.jpg"
 
     companion object {
         private var instance: TrackFileArtCache? = null
